4d2feead63c9e5feb9991ae0df81aa761c0aff85
tsbm112 <tsbm112@gmail.com>
2023-05-09 16:01:49 -0500

    change

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# CS472
+# CS472 (WAP)
+
+
+## Intro :sunglasses:
+
+Create a page where the user can type text into a box, and by clicking on UI controls, the user can decorate the text by giving it funny styling.
+
+
+## Extra Features :star:
+
+The signup form should have these elements:
+- Bigger decorations (with timer)
+- Bling and background image
+- on `Igpay Atinlay` button click, transform user input to `pig latin`.
+- on `Malkovitch` button click, user input words of >= 5 characters in length will be replaced with the word `Malkovich`.
+
+
+## Hosted on Github Pages :octocat:
+
+:link: [Link to github pages](https://chunkingz.github.io/CS472/)
--- decoratemytext.js
+++ decoratemytext.js
@@ -1,18 +1,92 @@
-// function alert1(){
-//     alert("Hello, world!");
-// } 
-function alert1() {
-    document.getElementById("text").style.fontSize = "24pt";
-}
-// function alert2(){
-//     alert("hi");
-// }
-function alert2() {
 
-    // $("check").attr("checkbox");
-    document.getElementById("text").style.fontWeight = "bold";
-    document.getElementById("text").style.color = "green";
-    document.getElementById("text").style.textDecoration = "underline";
+let decorationBtn = document.getElementById("bigger-decor-btn");
+let blingCheckBox = document.getElementById("bling");
+let userInput = document.getElementById("userInput");
+let pigLatin = document.getElementById("pig-latin-btn");
+let malkovitch = document.getElementById("malkovitch-btn");
+
+
+// btn1.addEventListener("click", () => {
+//     console.log("btn clicked");
+// })
+
+const incrementFontSize = () => {
+    let computedFontSize = window.getComputedStyle(userInput).getPropertyValue('font-size');
+    userInput.style.fontSize = (parseInt(computedFontSize) + 2) + 'px';
 }
 
+decorationBtn.onclick = () => {
+    if(String(userInput.value).trim() == "") return;
+    userInput.style.fontSize = '24pt';
+    userInput.style.textAlign = 'right';
+    setInterval(incrementFontSize, 500);
+}
+
+blingCheckBox.onclick = () => {
+    if(blingCheckBox.checked){
+        userInput.style.fontWeight = 'bold';
+        userInput.style.color = 'green';
+        userInput.style.textDecoration = 'underline';
+        userInput.style.textAlign = 'right';
+        document.body.style.backgroundImage = "url('https://courses.cs.washington.edu/courses/cse190m/CurrentQtr/labs/6/hundred-dollar-bill.jpg')";
+    } else {
+        userInput.style.fontWeight = 'normal';
+        userInput.style.color = 'initial';
+        userInput.style.textDecoration = 'none';
+        userInput.style.textAlign = 'left';
+        document.body.style.backgroundImage = "";
+    }
+}
+
+// hackers only section
+
+pigLatin.onclick = () => {
+    if(String(userInput.value).trim() == "") return;
+    const vowels = ['a','e','i','o','u'];
+    
+    let inputArray = String(userInput.value).split(' ');
+
+    // vowel checker
+    for(let i = 0; i < inputArray.length; i++){
+        let startsWithVowel = false;
+
+        for (let j = 0; j < vowels.length; j++) {
+            if(inputArray[i].startsWith(vowels[j])) {
+                inputArray[i] += 'ay';
+                startsWithVowel = true;
+                break;
+            }
+        }
+
+        if(startsWithVowel == false) {
+            // the current word (inputArray[i]) starts with a consonant
+            
+            let temp = inputArray[i].charAt(0);
+            // let counter = 0;
+            // for (let k = 0; k < vowels.length; k++) {
+            //     if(inputArray[i].charAt(k+1).startsWith(vowels[k])){
+            //         break;
+            //     } else {
+            //         temp += inputArray[i].charAt(k+1);
+            //         counter += k+1;
+            //     }
+            // }
+            inputArray[i] = inputArray[i].substring(1) + temp + 'ay';
+        }
+    }
+
+    userInput.value = inputArray.join(' ');
+}
+
+malkovitch.onclick = () => {
+    if(String(userInput.value).trim() == "") return;
+
+    let inputArray = String(userInput.value).split(' ');
+
+    for(let i=0; i<inputArray.length; i++){
+        if(inputArray[i].length >= 5) inputArray[i] = "Malkovich";
+    }
+
+    userInput.value = inputArray.join(' ');
+}
 
