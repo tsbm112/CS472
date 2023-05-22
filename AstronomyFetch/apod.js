e8cba6990fad7782abf723ae840cbc0e88ef90d3
tsbm112 <tsbm112@gmail.com>
2023-05-21 21:48:26 -0500

    new

---
+++ AstronomyFetch/apod.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+  $("#view_button").click(getPicture);
+  console.log('apod');
+});
+function getPicture() {
+
+
+var url=
+"https://api.nasa.gov/planetary/apod?api_key=gyhaJ2GQ5PF58E8w6iESVvscg2en3nrGcOqczI2s&date="+ $("#date").val();
+
+
+let fe=fetch(url);
+
+fe.then(res=> res.json())
+.then(d=>{
+   showPicture(d);
+    //error: noPicture,
+})
+.catch(d=>{
+  noPicture(d);
+   //error: noPicture,
+});
+console.log('getpic');
+
+};
+
+
+
+function showPicture(data) {
+  console.log('picture find')
+  $("#pic").attr("src", data.url);
+  $("#title").text(data.title);
+}
+function noPicture(error) {
+  console.log('picture not find')
+  alert(error.responseText);
+}
